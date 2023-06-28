e71aaafaf3338140402a8acdbfe23b963a2f45fc
Angel Segarra <angel.segara@gmail.com>
2023-06-28 15:13:56 -0400

    fish changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,17 @@
-if status is-interactive
-    set -g fish_greeting
+if not status --is-interactive
+    exit
+end
+
+set EDITOR nvim
+
+set -g fish_greeting
+
+if test -e ~/.asdf/asdf.fish
     source ~/.asdf/asdf.fish
 end
+
+alias wgu="sudo wg-quick up wg0"
+alias wgd="sudo wg-quick down wg0"
+
+zoxide init fish | source
+starship init fish | source
