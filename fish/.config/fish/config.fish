3dcdf5623ab831143fd5ffdf376f4156aa6453e5
Angel Segarra <angel.segara@gmail.com>
2023-07-01 11:24:42 -0400

    fix export fish env variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        set -g XDG_CURRENT_DESKTOP sway
-        set -g XDG_SESSION_TYPE wayland
-        set -g MOZ_ENABLE_WAYLAND 1
+        set -gx XDG_CURRENT_DESKTOP sway
+        set -gx XDG_SESSION_TYPE wayland
+        set -gx MOZ_ENABLE_WAYLAND 1
         keychain --eval --noask --quiet id_ed25519 | source
         exec sway >/tmp/sway.log 2>&1
     end
@@ -13,7 +13,7 @@ if not status --is-interactive
     exit
 end
 
-set EDITOR nvim
+set -gx EDITOR nvim
 
 set -g fish_greeting
 
