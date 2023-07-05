3c9ae204cd7b39be5c5a28317dd2ab864be46525
Angel Segarra <angel.segara@gmail.com>
2023-07-05 00:51:31 -0400

    change terminal and nvim theme

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -1,6 +1,12 @@
 return {
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").load()
+    end,
+  },
   "LazyVim/LazyVim",
   opts = {
-    colorscheme = "tokyonight-night",
+    colorscheme = "onedark",
   },
 }
