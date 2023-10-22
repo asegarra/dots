807209fcbd393560b01416bcb7d671e474784b9b
Angel Segarra <angel.segarra@gmail.com>
2023-10-22 15:56:35 -0400

    nvim eyecandy

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -22,4 +22,21 @@ return {
             },
         },
     },
+
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+                inc_rename = true,
+            }
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        }
+    }
 }
