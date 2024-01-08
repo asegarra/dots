a61613949cc16578788f2f47f44dc6c2bcd510c7
Angel Segarra <angel.segarra@gmail.com>
2024-01-07 23:22:20 -0500

    fish updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,35 +1,20 @@
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        set -gx XDG_CURRENT_DESKTOP sway
-        set -gx XDG_SESSION_TYPE wayland
-        set -gx MOZ_ENABLE_WAYLAND 1
-        set -gx VDPAU_DRIVER radeonsi
-        keychain --eval --noask --quiet id_ed25519 | source
-        exec sway >/tmp/sway.log 2>&1
-    end
-end
-
 if not status --is-interactive
     exit
 end
 
-set -gx EDITOR nvim
+fish_add_path ~/.local/share/coursier/bin
+fish_add_path ~/.local/bin
 
 set -g fish_greeting
+# set -gx EDITOR nvim
 
-if test -e ~/.asdf/asdf.fish
-    source ~/.asdf/asdf.fish
-end
-
-if test -e /usr/share/doc/pkgfile/command-not-found.fish
-    source /usr/share/doc/pkgfile/command-not-found.fish
-end
-
-alias wgu="sudo wg-quick up wg0"
-alias wgd="sudo wg-quick down wg0"
 alias lg="lazygit"
 alias pn="pnpm"
-alias man="batman"
+alias j="journalctl -f"
+alias ls="eza"
+
+abbr --add dotdot --regex '^\.\.+$' --function multicd
+abbr --add !! --position anywhere --function last_history_item
 
 zoxide init fish | source
 fnm env --use-on-cd | source
