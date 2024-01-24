b52403e986cac5c40f1acf5622d24d0eac39151a
Angel Segarra <angel.segarra@gmail.com>
2024-01-23 23:07:16 -0500

    zsh: udpates

--- zsh/.config/zsh/.zprofile
+++ zsh/.config/zsh/.zprofile
@@ -1,10 +1,16 @@
-# eval $(keychain --eval --noask --quiet id_ed25519)
-#
-# # Start graphical wayland session
-# if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-#   export XDG_CURRENT_DESKTOP=sway
-#   export XDG_SESSION_TYPE=wayland
-#   export MOZ_ENABLE_WAYLAND=1
-#   exec sway > /tmp/sway.log 2>&1
-# fi
+export EDITOR='nvim'
+export VISUAL='nvim'
 
+# Ensure path arrays do not contain duplicates.
+typeset -gU cdpath fpath mailpath path
+
+# Set the list of directories that Zsh searches for programs.
+path=(
+  $HOME/{,s}bin(N)
+  $HOME/.local/bin
+  /opt/local/{,s}bin(N)
+  /usr/local/{,s}bin(N)
+  $HOME/.cargo/bin
+  $HOME/.local/share/coursier/bin
+  $path
+)
--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -1,33 +1,5 @@
-export EDITOR='vim'
-export VISUAL='vim'
-export PAGER='less'
-
-# Ensure path arrays do not contain duplicates.
-typeset -gU cdpath fpath mailpath path
-
-# Set the list of directories that Zsh searches for programs.
-path=(
-  $HOME/{,s}bin(N)
-  $HOME/.local/bin
-  /opt/local/{,s}bin(N)
-  /usr/local/{,s}bin(N)
-  $HOME/.cargo/bin
-  $HOME/.local/share/coursier/bin
-  $path
-)
-
 # Define LS_COLORS
 eval "$(dircolors --sh)"
-
-export LESS='-g -i -M -R -S -w -z-4'
-
-export LESS_TERMCAP_mb=$'\E[01;31m'      # Begins blinking.
-export LESS_TERMCAP_md=$'\E[01;31m'      # Begins bold.
-export LESS_TERMCAP_me=$'\E[0m'          # Ends mode.
-export LESS_TERMCAP_se=$'\E[0m'          # Ends standout-mode.
-export LESS_TERMCAP_so=$'\E[00;47;30m'   # Begins standout-mode.
-export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
-export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
 
 ## History file configuration
 HISTFILE="$HOME/.zsh_history"
