19c86bbd9923b26db1c7e7151a3cd681ea85e3e3
Angel Segarra <angel.segarra@gmail.com>
2024-02-05 22:20:16 -0500

    nvim: switch back to CoC and change themes

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,25 +1,14 @@
 return {
-  -- Useful plugin to show you pending keybinds.
-  { "folke/which-key.nvim", opts = {} },
-
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = false,
-    priority = 1000,
+  { 
+    "sainnhe/gruvbox-material", 
+    priority = 1000, 
     config = function()
-      require('github-theme').setup({
-        specs = {
-          github_dark = {
-            bg1 = '#232627',
-            bg0 = '#232627',
-            inactive = '#31363b',
-          },
-        },
-      })
-      vim.cmd.colorscheme("github_dark")
+      vim.o.background = "dark"
+      vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
-
   {
     -- Set lualine as statusline
     "nvim-lualine/lualine.nvim",
