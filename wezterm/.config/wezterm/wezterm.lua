3143c962516ca43a8763c729ed3a6c25378540b8
Angel Segarra <angel.segarra@gmail.com>
2024-02-13 16:39:25 -0500

    updates

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,12 +4,14 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('FiraCode Nerd Font')
+config.font = wezterm.font('BlexMono Nerd Font Medium')
 config.font_size = 10.0
 --config.freetype_load_flags = 'DEFAULT'
 config.freetype_load_target = 'Light'
 config.freetype_render_target = 'HorizontalLcd'
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
-config.color_scheme = 'Gruvbox Material (Gogh)'
+config.color_scheme = 'Catppuccin Frappe'
 --config.color_scheme = 'Tokyo Night Storm'
 return config
