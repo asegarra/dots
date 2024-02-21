5bdea80defea93823a4244393dea34cc85a6ea22
Angel Segarra <angel.segarra@gmail.com>
2024-02-21 18:11:55 -0500

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,14 +4,17 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('JetBrainsMono NF Medium')
+config.font = wezterm.font { family = 'Hasklug Nerd Font', weight = 'Medium' }
 config.font_size = 10.0
---config.freetype_load_flags = 'DEFAULT'
+config.freetype_load_flags = 'DEFAULT'
 config.freetype_load_target = 'Light'
 config.freetype_render_target = 'HorizontalLcd'
 config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = false
 
 config.color_scheme = 'Catppuccin Frappe'
---config.color_scheme = 'Tokyo Night Storm'
+config.window_frame = {
+  font = wezterm.font { family = 'Cantarell', weight = 'Bold' },
+  font_size = 10.0,
+}
+
 return config
