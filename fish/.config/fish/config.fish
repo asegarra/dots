4944f7c61b8e30bb634747a09e8dc373c3f4ee27
Angel Segarra <angel.segarra@gmail.com>
2024-02-21 18:12:06 -0500

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,7 @@ fish_add_path ~/.local/share/coursier/bin
 fish_add_path ~/.local/bin
 
 set -g fish_greeting
-# set -gx EDITOR nvim
+set -gx EDITOR nvim
 
 alias lg="lazygit"
 alias j="journalctl -f"
@@ -15,9 +15,9 @@ alias ls="eza"
 abbr --add dotdot --regex '^\.\.+$' --function multicd
 abbr --add !! --position anywhere --function last_history_item
 
-function echo_prompt --on-event fish_postexec
-  echo ""
-end
+# function echo_prompt --on-event fish_postexec
+#   echo ""
+# end
 
 zoxide init fish | source
 fnm env --use-on-cd | source
