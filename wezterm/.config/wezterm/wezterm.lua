4f2cdb6ab7a17c15eb7d086070156587d3f7bf52
Angel Segarra <angel.segarra@gmail.com>
2024-03-30 12:34:12 -0400

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,12 +9,15 @@ config.font_size = 10.0
 config.freetype_load_flags = 'DEFAULT'
 config.freetype_load_target = 'Light'
 config.freetype_render_target = 'HorizontalLcd'
-config.hide_tab_bar_if_only_one_tab = true
-
 config.color_scheme = 'Catppuccin Frappe'
+
+-- config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
+-- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+-- config.integrated_title_button_style = "Gnome"
+-- config.integrated_title_buttons = { "Close" }
 config.window_frame = {
-  font = wezterm.font { family = 'Cantarell', weight = 'Bold' },
+  font = wezterm.font { family = 'Noto Sans', weight = 'Bold' },
   font_size = 10.0,
 }
 
