78fcaeab6a2bcac05d416bf05ec99bb0a7475020
Angel Segarra <angel.segarra@gmail.com>
2024-05-21 18:05:33 -0400

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,15 +2,19 @@ if not status --is-interactive
     exit
 end
 
-fish_add_path ~/.local/share/coursier/bin
-fish_add_path ~/.local/bin
-
 set -g fish_greeting
 set -gx EDITOR nvim
+set -gx N_PREFIX ~/n
+
+fish_add_path ~/.local/share/coursier/bin
+fish_add_path ~/.local/bin
+fish_add_path $N_PREFIX/bin
+fish_add_path /usr/local/go/bin
+fish_add_path ~/go/bin
+
 
 alias lg="lazygit"
 alias j="journalctl -f"
-alias ls="eza"
 
 abbr --add dotdot --regex '^\.\.+$' --function multicd
 abbr --add !! --position anywhere --function last_history_item
@@ -20,6 +24,5 @@ abbr --add !! --position anywhere --function last_history_item
 # end
 
 zoxide init fish | source
-fnm env --use-on-cd | source
-starship init fish | source
-
+# fnm env --use-on-cd | source
+# starship init fish | source
