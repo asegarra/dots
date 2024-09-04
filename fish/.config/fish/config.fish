0ca8972291feb91088ae60b5df84e9b89182df1a
Angel Segarra <angel.segarra@gmail.com>
2024-09-03 20:42:52 -0400

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,11 +4,9 @@ end
 
 set -g fish_greeting
 set -gx EDITOR nvim
-set -gx N_PREFIX ~/n
 
 fish_add_path ~/.local/share/coursier/bin
 fish_add_path ~/.local/bin
-fish_add_path $N_PREFIX/bin
 fish_add_path /usr/local/go/bin
 fish_add_path ~/go/bin
 
@@ -19,10 +17,6 @@ alias j="journalctl -f"
 abbr --add dotdot --regex '^\.\.+$' --function multicd
 abbr --add !! --position anywhere --function last_history_item
 
-# function echo_prompt --on-event fish_postexec
-#   echo ""
-# end
-
 zoxide init fish | source
-# fnm env --use-on-cd | source
-# starship init fish | source
+fnm env --use-on-cd | source
+starship init fish | source
